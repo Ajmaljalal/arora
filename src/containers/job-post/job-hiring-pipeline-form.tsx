525b592ecf35444952492977f728160a32d38658
ajmaljalal <jalalajmal96@gmail.com>
2021-03-29 18:51:13 -0700

    job post hiring pipeline form

---
+++ src/containers/job-post/job-hiring-pipeline-form.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface HiringPipelineProps {
+}
+
+const HiringPipeline = (props: HiringPipelineProps) => {
+  return (
+    <div>
+      hiring pipline
+    </div>
+  );
+}
+
+export default HiringPipeline
