e2ecd1e15355bc39dd3f7ebc75310042a2d6c8bc
ajmaljalal <jalalajmal96@gmail.com>
2021-08-01 00:11:48 -0700

    job summary form input field values

--- src/containers/create-job-post/job-summary-form.tsx
+++ src/containers/create-job-post/job-summary-form.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 import DatePicker from '../../components/forms/date-picker'
 import InputField from '../../components/forms/input'
 import SelectField from '../../components/forms/select'
+import { JobSummaryObject } from './utils'
 
 type JobSummaryFormProps = {
-  onChange?: () => void
+  data: JobSummaryObject
+  errors: any
+  onChange: (e) => void
 }
 
 const jobTypes = [
@@ -22,24 +25,23 @@ const jobMethods = [
 
 
 
-const JobSummaryForm = ({ onChange }: JobSummaryFormProps) => {
+const JobSummaryForm = ({ onChange, errors, data }: JobSummaryFormProps) => {
   return (
     <>
-      <InputField onChange={onChange} label='Job title' />
+      <InputField value={data?.JobTitle} onChange={onChange} label='Job Title' isRequired={true} isInvalid={errors?.titleErr?.length > 0} errorMessage={errors?.titleErr} />
       <Box display='flex' justifyContent='space-between'>
-        <SelectField onChange={onChange} label='Job Type' width='48%' options={jobTypes} />
-        <SelectField onChange={onChange} label='Job method' width='48%' options={jobMethods} />
+        <SelectField value={data?.JobType} onChange={onChange} label='Job Type' width='48%' options={jobTypes} isRequired={true} isInvalid={errors?.typeErr?.length > 0} errorMessage={errors?.typeErr} />
+        <SelectField value={data?.JobMethod} onChange={onChange} label='Job Method' width='48%' options={jobMethods} isRequired={true} isInvalid={errors?.methodErr?.length > 0} errorMessage={errors?.methodErr} />
       </Box>
       <Box display='flex' justifyContent='space-between'>
-        <InputField onChange={onChange} label='Job location' width='48%' />
-        <InputField onChange={onChange} label='Application deadline' type='date' width='48%' />
-
+        <InputField value={data?.JobLocation} onChange={onChange} label='Job Location' width='48%' isRequired={true} isInvalid={errors?.locationErr?.length > 0} errorMessage={errors?.locationErr} />
+        <InputField value={data?.ApplicationDeadline} onChange={onChange} label='Application Deadline' type='date' width='48%' isRequired={true} isInvalid={errors?.deadlineErr?.length > 0} errorMessage={errors?.deadlineErr} />
       </Box>
       <Box display='flex' justifyContent='space-between'>
-        <InputField onChange={onChange} label='Minimum salary' width='48%' leftAddOn='$' type='number' />
-        <InputField onChange={onChange} label='Maximum salary' width='48%' leftAddOn='$' type='number' />
+        <InputField value={data?.Minsalary} onChange={onChange} label='Min Salary' width='48%' leftAddOn='$' type='number' isRequired={true} isInvalid={errors?.minSalaryErr?.length > 0} errorMessage={errors?.minSalaryErr} />
+        <InputField value={data?.MaxSalary} onChange={onChange} label='Max Salary' width='48%' leftAddOn='$' type='number' isRequired={true} isInvalid={errors?.maxSalaryErr?.length > 0} errorMessage={errors?.maxSalaryErr} />
       </Box>
-      <InputField onChange={onChange} label='Minimum Years of experience (digits only)' type='number' />
+      <InputField value={data?.MinYearsofExperience} onChange={onChange} label='Min Years of Experience' type='number' isRequired={true} isInvalid={errors?.experienceErr?.length > 0} errorMessage={errors?.experienceErr} />
 
     </>
   )
