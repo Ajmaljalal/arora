d974428218ba56a2babfcfec3699c5eeabe57225
ajmaljalal <jalalajmal96@gmail.com>
2022-01-09 22:44:53 -0800

    centered icon button

---
+++ components/buttons/centered-icon-button.tsx
@@ -0,0 +1,23 @@
+import { IconButton } from '@chakra-ui/react';
+import React from 'react';
+
+export interface CenteredIconButtonProps {
+  bg?: string
+  Icon: JSX.Element
+  isRound?: boolean
+  onClick: () => void
+}
+
+export const CenteredIconButton = ({ bg = 'brand.white', Icon, isRound = false }: CenteredIconButtonProps) => {
+  return (
+    <IconButton
+      color='green'
+      aria-label='icon-button'
+      bg={bg}
+      icon={Icon}
+      size='sm'
+      isRound={isRound}
+      borderRadius={!isRound ? '3px' : '50%'}
+    />
+  );
+}
