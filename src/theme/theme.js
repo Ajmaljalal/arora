6ff44c5574238e8971e13feaf25c53c8a150f052
ajmaljalal <jalalajmal96@gmail.com>
2022-02-26 22:59:20 -0800

    icons color changes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,7 +4,7 @@ import { extendTheme } from "@chakra-ui/react"
 // 2. Call `extendTheme` and pass your custom values
 const theme = extendTheme({
   config: {
-    initialColorMode: 'dark',
+    initialColorMode: 'light',
     useSystemColorMode: false,
   },
   colors: {
