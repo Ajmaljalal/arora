f8795ed04344551ffdcefa4720d732633dc85b0c
ajmaljalal <jalalajmal96@gmail.com>
2022-03-13 21:30:51 -0700

    header searcbar and clientoption components implementation

--- src/containers/header/header.tsx
+++ src/containers/header/header.tsx
@@ -1,73 +1,17 @@
-import React, { useState } from 'react'
-import { Avatar, Box, Button, Divider, HStack, Input, InputGroup, InputLeftAddon, Menu, MenuButton, MenuItem, MenuList, useColorMode, useColorModeValue } from '@chakra-ui/react'
-import { BaseButton } from '../../components/buttons/button'
-import ChevronDown from '../../../public/assets/icons/chevron-down.svg'
-import SearchIcon from '../../../public/assets/icons/search.svg'
+import React from 'react'
+import { Avatar, Box, Divider, HStack, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import AddIcon from '../../../public/assets/icons/add-white.svg'
 import NotificationIcon from '../../../public/assets/icons/notification.svg'
 import { CenteredIconButton } from '../../components/buttons/centered-icon-button'
+import SearchBar from './header-searchbar'
+import ClientsDropDown from './header-client-options'
 
-function Header() {
+const Header = () => {
 	const { colorMode, toggleColorMode } = useColorMode()
 	const borderBottomColor = useColorModeValue('grey', 'lightGray')
-	const [searchOnFocus, setSearchOnFocus] = useState(false)
 
-	const menu = (
-		<Menu>
-			<MenuButton as={Button} rightIcon={<ChevronDown />}>
-				Actions
-			</MenuButton>
-			<MenuList>
-				<MenuItem>Download</MenuItem>
-				<MenuItem>Create a Copy</MenuItem>
-				<MenuItem>Mark as Draft</MenuItem>
-				<MenuItem>Delete</MenuItem>
-				<MenuItem>Attend a Workshop</MenuItem>
-			</MenuList>
-		</Menu>
-	)
-	return (
-		<Box
-			height='72px'
-			width='100%'
-			bg='brand.white'
-			borderBottom={`1px solid ${borderBottomColor}`}
-			display='flex'
-			alignItems='center'
-			px='32px'
-			color='brand.black'
-		>
-			<BaseButton
-				text='Cleint: Goldman Sachs'
-				color='brand.grey500'
-				onClick={() => console.log('clicked')}
-				outlined
-				borderColor='brand.grey200'
-				icon={<ChevronDown />}
-				iconPosition='right'
-			/>
-			<Divider orientation='vertical' borderColor='brand.grey200' mx='30px' />
-			<InputGroup
-				mr='50px'
-				border='1px solid transparent'
-				borderRadius='3px'
-				_hover={{ border: '1px solid lightgrey', background: 'brand.grey100' }}
-			>
-				<InputLeftAddon
-					children={searchOnFocus ? menu : <SearchIcon />}
-					p='0 10px'
-				/>
-				<Input
-					type='search'
-					placeholder='Search Jobs, Candidates...'
-					variant='unstyled'
-					p='0 10px'
-					boxSizing='border-box'
-					colorScheme='brand.black'
-					_placeholder={{ color: 'brand.grey300' }}
-					onFocus={() => setSearchOnFocus(true)}
-				/>
-			</InputGroup>
+	const rightSideQuickActions = () => {
+		return (
 			<HStack>
 				<CenteredIconButton
 					Icon={<AddIcon />}
@@ -89,9 +33,32 @@ function Header() {
 					src='https://bit.ly/tioluwani-kolawole'
 				/>
 			</HStack>
+		)
+	}
+
+
+	// Component return
+	return (
+		<Box
+			height='72px'
+			width='100%'
+			bg='brand.white'
+			borderBottom={`1px solid ${borderBottomColor}`}
+			display='flex'
+			alignItems='center'
+			px='32px'
+			color='brand.black'
+		>
+			<ClientsDropDown />
+			<Divider orientation='vertical' borderColor='brand.grey200' mx='30px' />
+			<SearchBar />
+			{rightSideQuickActions()}
 
 		</Box>
 	)
 }
+
+
+
 
 export default Header
--- src/containers/nav/utils/index.tsx
+++ src/containers/nav/utils/index.tsx
@@ -1,9 +1,9 @@
-import Home from '../../../../public/assets/icons/home.svg'
-import Dashboard from '../../../../public/assets/icons/dashboard.svg'
-import Inbox from '../../../../public/assets/icons/inbox.svg'
-import Jobs from '../../../../public/assets/icons/jobs.svg'
-import Candidates from '../../../../public/assets/icons/candidates.svg'
-import Settings from '../../../../public/assets/icons/settings.svg'
+import Home from '../../../../public/assets/icons/navigation/home.svg'
+import Dashboard from '../../../../public/assets/icons/navigation/dashboard.svg'
+import Inbox from '../../../../public/assets/icons/navigation/inbox.svg'
+import Jobs from '../../../../public/assets/icons/navigation/jobs.svg'
+import Candidates from '../../../../public/assets/icons/navigation/candidates.svg'
+import Settings from '../../../../public/assets/icons/navigation/settings.svg'
 
 export type NavItem = {
   text: string
