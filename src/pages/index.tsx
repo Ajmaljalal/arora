ab96dc38ace07b9b7e73a084d911b2341fcf5a0f
ajmaljalal <jalalajmal96@gmail.com>
2022-04-05 23:37:54 -0700

    Dashboard page Job status card item list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { Box } from '@chakra-ui/react'
 import Head from 'next/head'
 import PageTitle from '../components/headings/page-title'
+import PageContentWrapper from '../components/layouts/page-content-wrapper'
 import Body from '../containers/body'
+import JobStatusCardsList from '../containers/dashboard/jobs-status-cards-list'
 import BodyLayout from '../containers/layouts/body-layout'
 
 
@@ -15,6 +17,9 @@ export default function Home() {
       <BodyLayout>
         <Body>
           <PageTitle text='Dashboard' />
+          <PageContentWrapper>
+            <JobStatusCardsList />
+          </PageContentWrapper>
         </Body>
       </BodyLayout>
     </Box>
