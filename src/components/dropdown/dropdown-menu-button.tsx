6acc7cac546af95c905108d8ce533735a8f55b0d
ajmaljalal <jalalajmal96@gmail.com>
2022-04-24 00:18:43 -0700

    dropdown menu button component

---
+++ src/components/dropdown/dropdown-menu-button.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button, MenuButton } from '@chakra-ui/react'
+import ChevronDown from '../../../public/assets/icons/chevron-down.svg'
+
+type DropdownMenuButtonProps = {
+  text: string
+}
+
+const DropdownMenuButton = ({ text }: DropdownMenuButtonProps) => {
+  return (
+    <MenuButton
+      as={Button}
+      rightIcon={<ChevronDown />}
+      p='0 10px'
+      borderRadius='3px'
+      border='1px solid'
+      borderColor='brand.grey200'
+      minWidth='250px'
+      textAlign='start'
+      fontWeight='400'
+      _focus={{ boxShadow: "none !important" }}
+    >
+      {text}
+    </MenuButton>
+  )
+}
+
+export default DropdownMenuButton
