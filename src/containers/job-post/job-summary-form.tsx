252dbdcf7cb379522f06150a04ae4a9a0a507905
ajmaljalal <jalalajmal96@gmail.com>
2022-05-17 16:52:01 -0700

    select component

--- src/containers/job-post/job-summary-form.tsx
+++ src/containers/job-post/job-summary-form.tsx
@@ -1,23 +1,46 @@
-import { Box, FormControl, FormErrorMessage, FormLabel, HStack, Input } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import DatePicker from '../../components/forms/date-picker'
 import InputField from '../../components/forms/input'
+import SelectField from '../../components/forms/select'
 
 type JobSummaryFormProps = {
   onChange?: () => void
 }
+
+const jobTypes = [
+  'Full time',
+  'Part time',
+  'Contract'
+]
+
+const jobMethods = [
+  'Remote | work from home',
+  'In person | work from office',
+  'Hybrid | work from home and office'
+]
+
+
 
 const JobSummaryForm = ({ onChange }: JobSummaryFormProps) => {
   return (
     <>
       <InputField onChange={onChange} label='Job title' />
       <Box display='flex' justifyContent='space-between'>
-        <InputField onChange={onChange} label='Job type' width='48%' />
-        <InputField onChange={onChange} label='Job method' width='48%' />
+        <SelectField onChange={onChange} label='Job Type' width='48%' options={jobTypes} />
+        <SelectField onChange={onChange} label='Job method' width='48%' options={jobMethods} />
       </Box>
-      <InputField onChange={onChange} label='Job location' />
-      <InputField onChange={onChange} label='Salary' />
-      <InputField onChange={onChange} label='Minimum required experience' />
-      <InputField onChange={onChange} label='Application deadline' />
+      <Box display='flex' justifyContent='space-between'>
+        <InputField onChange={onChange} label='Job location' width='48%' />
+        <InputField onChange={onChange} label='Application deadline' type='date' width='48%' />
+
+      </Box>
+      <Box display='flex' justifyContent='space-between'>
+        <InputField onChange={onChange} label='Minimum salary' width='48%' leftAddOn='$' type='number' />
+        <InputField onChange={onChange} label='Maximum salary' width='48%' leftAddOn='$' type='number' />
+      </Box>
+      <InputField onChange={onChange} label='Minimum Years of experience (digits only)' type='number' />
+
     </>
   )
 }
