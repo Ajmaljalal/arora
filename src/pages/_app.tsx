a66f5688038f89bb6d1a4dca6451f2a536b6a359
ajmaljalal <jalalajmal96@gmail.com>
2022-05-22 14:48:08 -0700

    _document and _app files head updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme/theme'
 import Layout from '../components/layouts'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
         <Layout>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
