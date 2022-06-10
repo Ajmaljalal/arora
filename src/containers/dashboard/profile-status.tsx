0fd017ea0cfe5f5c516afe008ee249a153db11d0
ajmaljalal <jalalajmal96@gmail.com>
2022-06-09 23:58:35 -0700

    tile with header profile status

---
+++ src/containers/dashboard/profile-status.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TileWithHeader from '../../components/tiles/tile-with-header'
+
+type Props = {}
+
+const ProfileStatus = () => {
+  return (
+    <TileWithHeader title='Complete your profile' />
+  )
+}
+
+export default ProfileStatus
